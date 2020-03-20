a5dda60623b4510e07287d3bd8bc1a798f323fbd
chiru-hub <56336144+chiru-hub@users.noreply.github.com>
2020-03-20 13:27:33 +0530

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style lang='scss'>
+@font-face {
+  font-family: "Poppings";
+  src: url("/static/fonts/Poppins-Light.ttf"); /* IE9 Compat Modes */
+  font-weight: 300;
+}
+@font-face {
+  font-family: "Poppins";
+  src: url("/fonts/Poppins-Regular.ttf"); /* IE9 Compat Modes */
+  font-weight: 400;
+}
+@font-face {
+  font-family: "Poppings";
+  src: url("/fonts/Poppins-Bold.ttf"); /* IE9 Compat Modes */
+  font-weight: 700;
+}
+
+html {
+  font-size: 62.5%;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: "Poppins", sans-serif;
+  font-size: 1.6rem;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="first-part">
+      <div class="container">
+        <div class="pt-4">
+          <h1 class="flex justify-center">Why MicMellows?</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped lang='scss'>
+.first-part {
+  background-image: linear-gradient(#cd1e01 70%, #f8650c 30%);
+}
+</style>
