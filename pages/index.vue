64985dfafd8701dba4b358f8215d49010794b218
chiru-hub <56336144+chiru-hub@users.noreply.github.com>
2020-04-02 02:46:24 +0530

    parts addded

--- pages/index.vue
+++ pages/index.vue
@@ -8,18 +8,28 @@
             class="text-left sm:w-auto md:w-3/5 mt-10 mx-auto"
           >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt officia nemo quasi harum praesentium recusandae porro odit doloribus, quod at.</p>
         </div>
-        <div class="px-0 md:px-64 pt-20 flex md:flex-row flex-col justify-between">
+        <div class="px-0 md:px-64 pb-40 pt-20 flex md:flex-row flex-col justify-between">
           <div class="first-part-cards"></div>
           <div class="first-part-cards"></div>
           <div class="first-part-cards"></div>
         </div>
         <div class="px-0 md:px-20">
-          <h1 class="text-center md:pt-32 md:pb-10">Featured Courses</h1>
-          <div class="flex md:flex-row flex-col justify-between">
+          <h1 class="text-center md:pb-20">Featured Courses</h1>
+          <div class="flex md:flex-row justify-between">
             <div class="first-part-cards2"></div>
             <div class="first-part-cards2"></div>
             <div class="first-part-cards2"></div>
           </div>
+        </div>
+      </div>
+    </div>
+    <div class="second-part">
+      <div class="lg:container pb-40">
+        <h1 class="text-center pt-24 pb-20">Our Events</h1>
+        <div class="flex md:flex-row justify-between px-0 md:px-20">
+          <div class="second-part-cards2"></div>
+          <div class="second-part-cards2"></div>
+          <div class="second-part-cards2"></div>
         </div>
       </div>
     </div>
@@ -47,6 +57,29 @@
     width: 21.8rem;
     background-color: white;
     border-radius: 8px;
+  }
+
+  &-cards2 {
+    width: 339px;
+    height: 435px;
+    object-fit: contain;
+    border-radius: 8px;
+    box-shadow: -12px 12px 30px 0 rgba(199, 93, 23, 0.16);
+    background-image: linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0),
+      rgba(4, 4, 4, 0) 22%,
+      #111111
+    );
+  }
+}
+
+.second-part {
+  background-image: linear-gradient(#cd1e01 60%, #f8650c 40%);
+  h1 {
+    color: white;
+    font-size: 4rem;
+    font-weight: 800;
   }
 
   &-cards2 {
